786c9bc780abd5076a874cdf4039455c0d8a3352
Brundha <118626584+brundha30@users.noreply.github.com>
2022-11-24 19:06:30 +0530

    Add files via upload

---
+++ addcrop.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 20 12:29:43 2022
+
+@author: LENOVO
+"""
+
+from tkinter import filedialog
+from tkinter import*
+import sqlite3
+from tkinter import messagebox
+
+top=Tk()
+top.geometry('1000x1000')
+top.title("Add Crop")
+
+crop_id=IntVar()
+crop_name=StringVar()
+cost_range=StringVar()
+description=StringVar()
+global file,blob_data
+def upload():
+    global file,blob_data
+    file = filedialog.askopenfilename()
+    fob=open(file,'rb')
+    blob_data=fob.read() # Binary data is ready   
+def database():
+    cropid1=crop_id.get()
+    cropname1=crop_name.get()
+    costrange1=cost_range.get()
+    description1=description.get()
+    
+    conn=sqlite3.connect('project.db')
+    with conn:
+        cursor=conn.cursor()
+    cursor.execute('INSERT INTO addcrop (Crop_ID,Crop_Name,Cost_Range,Description,Photo) VALUES(?,?,?,?,?)',(cropid1,cropname1,costrange1,description1,blob_data))
+    conn.commit()
+    messagebox.showinfo("Message","Data inserted Successfully")
+
+   
+def back():
+    top.destroy()
+    import adminhomepage
+
+
+label_0 = Label(top,text="ADD CROPS",width=30,font=("bold",30))
+label_0.place(x=70,y=20)
+
+label_1 = Label(top,text="Crop ID",width=20,font=("bold",20))
+label_1.place(x=90,y=110)
+
+entry_1=Entry(top,textvar=crop_id,width=30)
+entry_1.place(x=380,y=120)
+
+label_2 = Label(top,text="Crop Name",width=20,font=("bold",20))
+label_2.place(x=90,y=170)
+
+entry_2=Entry(top,textvar=crop_name,width=30)
+entry_2.place(x=380,y=180)
+
+label_3 = Label(top,text="Cost Range",width=20,font=("bold",20))
+label_3.place(x=90,y=230)
+
+entry_3=Entry(top,textvar=cost_range,width=30)
+entry_3.place(x=380,y=240)
+
+label_4 = Label(top,text="Description",width=20,font=("bold",20))
+label_4.place(x=90,y=290)
+
+entry_4=Entry(top,textvar=description,width=30)
+entry_4.place(x=380,y=300)
+
+
+label_5 = Label(top,text="Upload Image",width=20,font=("bold",20))
+label_5.place(x=90,y=350)
+
+
+
+button=Button(top,text="Browse",width=17,bg="white",fg="black",bd=4,font=("arial"),command=upload).place(x=380,y=350)
+
+button=Button(top,text="Submit",width=20,bg="green",fg="white",bd=5,command=database).place(x=200,y=550)
+
+button=Button(top,text="Back",width=20,bg="orange",fg="white",bd=5,command=back).place(x=470,y=550)
+
+
+top.mainloop()
